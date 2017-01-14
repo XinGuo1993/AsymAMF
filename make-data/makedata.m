cc1ec6a35574b5415f9afd15e542674c1af55ec3
Xin Guo <1219081995@qq.com>
2017-01-14 16:38:21 +0800

    Add files via upload

---
+++ make-data/makedata.m
@@ -0,0 +1,324 @@
+%生成160*160图片
+
+new_pic = zeros(160);
+for i =1:17
+    for j=1:160
+        new_pic(i,j)=255;
+    end
+end
+
+for i =20:21
+    for j=1:160
+        new_pic(i,j)=255;
+    end
+end
+
+for i =23:35
+    for j=2:11
+        new_pic(i,j)=156;
+    end
+    for j=13:43
+        new_pic(i,j)=99;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=99;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=189;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=255;
+    end   
+    for j=125:140
+        new_pic(i,j)=0;
+    end
+    for j=142:159
+        new_pic(i,j)=255;
+    end
+end
+
+for i =37:55
+    for j=2:11
+        new_pic(i,j)=255;
+    end
+    for j=13:43
+        new_pic(i,j)=255;
+    end
+    for j=45:55
+        new_pic(i,j)=156;
+    end
+    for j=57:60
+        new_pic(i,j)=255;
+    end
+    for j=62:87
+        new_pic(i,j)=99;
+    end    
+    for j=89:91
+        new_pic(i,j)=255;
+    end    
+    for j=93:103
+        new_pic(i,j)=189;
+    end   
+    for j=105:123
+        new_pic(i,j)=255;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=99;
+    end
+end
+
+for i =57:68
+    for j=2:11
+        new_pic(i,j)=99;
+    end
+    for j=13:43
+        new_pic(i,j)=156;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=189;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=255;
+    end    
+    for j=93:103
+        new_pic(i,j)=0;
+    end   
+    for j=105:123
+        new_pic(i,j)=255;
+    end   
+    for j=125:140
+        new_pic(i,j)=189;
+    end
+    for j=142:159
+        new_pic(i,j)=255;
+    end
+end
+
+for i =70:72
+    for j=2:11
+        new_pic(i,j)=255;
+    end
+    for j=13:43
+        new_pic(i,j)=255;
+    end
+    for j=45:55
+        new_pic(i,j)=189;
+    end
+    for j=57:60
+        new_pic(i,j)=255;
+    end
+    for j=62:87
+        new_pic(i,j)=99;
+    end    
+    for j=89:91
+        new_pic(i,j)=189;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=99;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=255;
+    end
+end
+
+for i =74:99
+    for j=2:11
+        new_pic(i,j)=255;
+    end
+    for j=13:43
+        new_pic(i,j)=189;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=0;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=255;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=189;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=99;
+    end
+end
+
+for i =101:104
+    for j=2:11
+        new_pic(i,j)=0;
+    end
+    for j=13:43
+        new_pic(i,j)=255;
+    end
+    for j=45:55
+        new_pic(i,j)=189;
+    end
+    for j=57:60
+        new_pic(i,j)=255;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=99;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=99;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=255;
+    end
+end
+
+
+for i =106:115
+    for j=2:11
+        new_pic(i,j)=255;
+    end
+    for j=13:43
+        new_pic(i,j)=255;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=0;
+    end
+    for j=62:87
+        new_pic(i,j)=99;
+    end    
+    for j=89:91
+        new_pic(i,j)=255;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=255;
+    end   
+    for j=125:140
+        new_pic(i,j)=0;
+    end
+    for j=142:159
+        new_pic(i,j)=255;
+    end
+end
+
+for i =117:148
+    for j=2:11
+        new_pic(i,j)=99;
+    end
+    for j=13:43
+        new_pic(i,j)=189;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=99;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=0;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=99;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=0;
+    end
+end
+
+for i =150:159
+    for j=2:11
+        new_pic(i,j)=255;
+    end
+    for j=13:43
+        new_pic(i,j)=0;
+    end
+    for j=45:55
+        new_pic(i,j)=255;
+    end
+    for j=57:60
+        new_pic(i,j)=189;
+    end
+    for j=62:87
+        new_pic(i,j)=255;
+    end    
+    for j=89:91
+        new_pic(i,j)=99;
+    end    
+    for j=93:103
+        new_pic(i,j)=255;
+    end   
+    for j=105:123
+        new_pic(i,j)=189;
+    end   
+    for j=125:140
+        new_pic(i,j)=255;
+    end
+    for j=142:159
+        new_pic(i,j)=99;
+    end
+end
+% B0 = (double(new_pic(:, :, 1)) - 128) / 128;  %无文字矩阵  
+% imshow(B0,[0,1])
+imwrite(new_pic/255,'160.bmp')
+
+pic = imread('2.bmp');
+D = (double(pic(:, :, 1)) - 128) / 128;   %有文字矩阵Y
+[m, n] = size(B0);
+S0 = (D == (41 - 128) / 128);  %是文字just a simple test,41是文字的值，S0为0，1矩阵,1在matlab中是白色
+new_s0 = imresize(S0,[160,160],'bicubic');
+% imshow(new_s0,[0,1])
+new_pic(new_s0)=41;
+imwrite(new_pic/255,'161.bmp')
